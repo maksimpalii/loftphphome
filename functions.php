e93d2d186b26b1b16df34685968c4ff9b7535ea7
Maksim Palii <maksim.palii@gmail.com>
2018-02-28 22:20:39 +0200

    2 - task#3

--- functions.php
+++ functions.php
@@ -2,10 +2,6 @@
 
 function task1($arr = array("строка 1"), $trigger = false)
 {
-    //$numargs = func_num_args();
-    //echo "Количество аргументов: $numargs\n";
-    //print_r(func_get_arg(0));
-
     if (is_bool($trigger) === true && $trigger == true) {
         $arr_line = implode(" ", $arr);
         return $arr_line;
@@ -14,7 +10,10 @@ function task1($arr = array("строка 1"), $trigger = false)
             echo "<p>$value</p>";
         }
     }
+    return 0;
 }
+
+
 
 function task2($arr, $action = NULL)
 {
@@ -22,11 +21,11 @@ function task2($arr, $action = NULL)
     {
         foreach ($arr as $key => $value) {
             if (is_numeric($value) == false) {
-               return false;
+                return false;
             }
         }
+        return 0;
     }
-
     if ($arr == NULL || count($arr) < 2) {
         echo "data array is NULL or < 2";
     } elseif (is_numArray($arr) === false) {
@@ -66,6 +65,73 @@ function task2($arr, $action = NULL)
             default:
                 echo "нет действия или не верное";
         }
+    }
+}
+
+function task3()
+{
+    function is_numerArray($newArr)
+    {
+        foreach ($newArr as $key => $value) {
+            if (is_numeric($value) == false) {
+                return false;
+            }
+        }
+        return 0;
+    }
+    $numargs = func_num_args();
+    $firstArg = func_get_arg(0);
+
+    if ($numargs > 2) {
+        //echo "ok";
+        if (is_string($firstArg)) {
+            //echo "певрое string";
+            $newArr = func_get_args();
+            unset($newArr[0]);
+            if (is_numerArray($newArr) === false) {
+                echo "не числа";
+            } else {
+                //echo "ok numeric" .PHP_EOL;
+                $result = $newArr[1];
+                switch ($firstArg) {
+                    case '-':
+                        for ($i = 2; $i <= count($newArr); $i++) {
+                            $result -= $newArr[$i];
+                        }
+                        echo $result;
+                        break;
+                    case '+':
+                        for ($i = 2; $i <= count($newArr); $i++) {
+                            $result += $newArr[$i];
+                        }
+                        echo $result;
+                        break;
+                    case '/':
+                        for ($i = 2; $i <= count($newArr); $i++) {
+                            if ($newArr[$i] == 0) {
+                                $result = "нв '0' делить нельзя!";
+                                break;
+                            } else {
+                                $result /= $newArr[$i];
+                            }
+                        }
+                        echo $result;
+                        break;
+                    case '*':
+                        for ($i = 2; $i <= count($newArr); $i++) {
+                            $result *= $newArr[$i];
+                        }
+                        echo $result;
+                        break;
+                    default:
+                        echo "нет действия или не верное";
+                }
+            }
+        } else {
+            echo "Первый агрумент не строка";
+        }
+    } else {
+        echo "мало данных";
     }
 }
 
