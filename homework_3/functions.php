498f561c485d8c4848fc53208cf904f3125cd903
Maksim Palii <maksim.palii@gmail.com>
2018-03-09 13:32:01 +0200

    home work 3

--- homework_3/functions.php
+++ homework_3/functions.php
@@ -42,7 +42,7 @@ function task1()
     <?php
     foreach ($xml->Address as $key => $value) {
         echo "<p><b>" . $value->attributes()->Type . ": </b>";
-        echo $value->Name . $value->Street . $value->City ;
+        echo $value->Name . $value->Street . $value->City;
         echo $value->State . $value->Zip . $value->Country . "</p>";
     }
 
@@ -91,5 +91,94 @@ function task1()
 //    echo $xml->Items->Item->USPrice->__toString() . PHP_EOL;
 //    echo $xml->Items->Item->ShipDate->__toString() . PHP_EOL;
 
+function task2()
+{
+    $product = ["ProductName" => "Lawnmower", "Quantity" => 1, "USPrice" => 148.95];
+    $data = ["Name" => "Ellen Adams", "Street" => "123 Maple Street", "Product" => $product];
+    $encoded = json_encode($data, JSON_UNESCAPED_UNICODE);
+    file_put_contents('output.json', $encoded);
+
+    $data2 = file_get_contents('output.json');
+    $decoded = json_decode($data2, true);
+
+    switch (random_int(1, 3)) {
+        case 1:
+            $decoded["Product"]["USPrice"] += random_int(3, 20);
+            $decoded["Product"]["Quantity"] += random_int(1, 4);
+            break;
+        case 2:
+            $decoded["Name"] = "Tai Yee";
+            break;
+        default:
+            echo "Данные не изменились!";
+    }
+
+    $encoded = json_encode($decoded, JSON_UNESCAPED_UNICODE);
+    file_put_contents('output2.json', $encoded);
+
+    $arr = file_get_contents('output.json');
+    $arr2 = file_get_contents('output2.json');
+
+    $arrDecoded = json_decode($arr, true);
+    $arr2Decoded = json_decode($arr2, true);
 
 
+    foreach ($arrDecoded as $value => $key) {
+        if ($arr2Decoded[$value] != $arrDecoded[$value]) {
+            if (is_array($arr2Decoded[$value])) {
+                foreach ($arrDecoded[$value] as $val => $key2) {
+                    if ($arr2Decoded[$value][$val] != $arrDecoded[$value][$val]) {
+                        echo $val . " => " . $arr2Decoded[$value][$val] . PHP_EOL;
+                    }
+                }
+            } else {
+                echo $value . " => " . $arr2Decoded[$value];
+            }
+        }
+    }
+
+}
+
+function task3()
+{
+    $csvPath = 'numbers.csv';
+    $data = [];
+    for ($i = 0; $i < 50; $i++) {
+        $data[$i] = random_int(1, 100);
+    }
+    $fp = fopen($csvPath, 'w');
+
+    fputcsv($fp, $data, ",");
+    fclose($fp);
+
+    $csvFile = fopen($csvPath, 'r');
+    if ($csvFile) {
+        $res = array();
+        while (($csvData = fgetcsv($csvFile, filesize($csvPath), ',')) !== false) {
+            $res[] = $csvData;
+        }
+        //print_r($res);
+        $summa = 0;
+        foreach ($res as $val) {
+            foreach ($val as $numb) {
+                if ($numb % 2 === 0) {
+                    $summa += $numb;
+                }
+            }
+        }
+        echo "Сумма четных чисел: " . $summa;
+    }
+}
+
+function task4()
+{
+
+    $data = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json');
+    $dataDecoded = json_decode($data, true);
+
+    // print_r($dataDecoded['query']['pages']['15580374']);
+    if (array_key_exists('pageid', $dataDecoded['query']['pages']['15580374'])) {
+        echo "pageid: " . $dataDecoded['query']['pages']['15580374']['pageid'] . PHP_EOL;
+        echo "Title: " . $dataDecoded['query']['pages']['15580374']['title'];
+    }
+}
--- homework_3/index.php
+++ homework_3/index.php
@@ -1,4 +1,10 @@
 <?php
 require "functions.php";
 
-task1();
+//task1();
+
+//task2();
+
+//task3();
+
+task4();
